4fc7a27e4a25acb9adc61ecd6450794ad8d2a5ab
khushal024 <khushalwebroot@gmail.com>
2022-09-26 23:35:45 +0530

    Angular first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
